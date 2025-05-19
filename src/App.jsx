067875c726b0d3b1880cdbd701ccfd70168fda8d
Serhii bc2024-6 <tickhere135@gmail.com>
2025-05-19 17:37:02 +0300

    correcting syntax

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export const alex = {
 export const App = () => (
   <div className="App">
     <Person person={misha} />
-    <Person person={olya} />;
-    <Person person={alex} />;
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
